836f0a79c4b31ccf99fab09f048b4b0c85cdbb97
chackymeow <fabichasola@hotmail.com>
2021-02-11 03:02:04 +0000

    avance

--- exercises/06.4-Loop-from-the-half-to-the-end/app.js
+++ exercises/06.4-Loop-from-the-half-to-the-end/app.js
@@ -1,6 +1,6 @@
 var mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
 
-var initialValue = mySampleArray/2-1;
+var initialValue = mySampleArray;
 var stopValue = 0;
 var increasingValue = 1;
 
