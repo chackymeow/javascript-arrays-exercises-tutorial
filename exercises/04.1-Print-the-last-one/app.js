06a51596a2db4089c0bdf83642bca5e63bf57350
chackymeow <fabichasola@hotmail.com>
2021-02-11 02:39:52 +0000

    avance hasta el 06.4

--- exercises/04.1-Print-the-last-one/app.js
+++ exercises/04.1-Print-the-last-one/app.js
@@ -5,7 +5,9 @@ function generateRandomArray()
 	for(var i = 0;i<randomLength;i++) auxArray.push(Math.floor(Math.random()*100));
 	return auxArray;	
 }
-var myStupidArray = generateRandomArray();
+let myStupidArray = generateRandomArray();
 
-let theLastOne= myStupidArray[myStupidArray.length];
+let theLastOne= myStupidArray[myStupidArray.length-1];
+console.log(theLastOne);
 //Your code here
+
--- exercises/06.4-Loop-from-the-half-to-the-end/app.js
+++ exercises/06.4-Loop-from-the-half-to-the-end/app.js
@@ -1,6 +1,6 @@
 var mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
 
-var initialValue = 0;
+var initialValue = mySampleArray/2-1;
 var stopValue = 0;
 var increasingValue = 1;
 
